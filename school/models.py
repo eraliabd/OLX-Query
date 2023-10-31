437b3de37d817453b435989b676ed971c3469bbf
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2023-10-31 17:27:40 +0500

    created models

--- school/models.py
+++ school/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class MonthNameChoices(models.TextChoices):
+    """Oy nomlari"""
+
     january = "January"
     february = "February"
     march = "March"
@@ -17,11 +19,16 @@ class MonthNameChoices(models.TextChoices):
 
 
 class Region(models.Model):
-    title = models.CharField(max_length=255)
-    ball = models.FloatField()
-    result = models.FloatField()
+    """Viloyatlar"""
 
-    month_choice = models.CharField(max_length=10, choices=MonthNameChoices, default=MonthNameChoices.january)
+    title = models.CharField(max_length=255)
+    ball = models.FloatField(null=True, blank=True)
+    avg_result = models.FloatField()
+    month_result = models.FloatField()
+
+    month_choice = models.CharField(
+        max_length=10, choices=MonthNameChoices, null=True, blank=True
+    )
 
     def __str__(self):
         return self.title
@@ -30,7 +37,7 @@ class Region(models.Model):
 class District(models.Model):
     """Tumanlar"""
 
-    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='districts')
 
     title = models.CharField(max_length=255)
     result = models.FloatField()
@@ -42,7 +49,7 @@ class District(models.Model):
 class School(models.Model):
     """Maktablar"""
 
-    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='schools')
 
     title = models.CharField(max_length=255)
     result = models.FloatField()
@@ -54,9 +61,18 @@ class School(models.Model):
 class Student(models.Model):
     """Maktab o'quvchilari"""
 
-    school = models.ForeignKey(School, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
-    result = models.FloatField()
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
+    full_name = models.CharField(max_length=255)
+    percentage = models.FloatField()
 
     def __str__(self):
-        return self.title
+        return self.full_name
+
+    @property
+    def ball(self):
+        if self.percentage <= 100 and self.percentage >= 80:
+            return 1
+        elif self.percentage < 80 and self.percentage >= 50:
+            return 0.5
+        else:
+            return 0
